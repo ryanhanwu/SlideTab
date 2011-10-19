40d877acad397c26578336009bb2a8aeef50c049
Flyworld <flyworld@gmail.com>
2011-10-19 14:08:27 +0800

    fine tune

--- src/jquery.slidetab-1.0.js
+++ src/jquery.slidetab-1.0.js
@@ -1,9 +1,17 @@
 (function($) {
 	$.fn.slideTab = function(o) {
+		var primaryClass = "slideTabs";
 		o = $.extend({
-			class_main : "slideTabs",
-			class_selected : "selected",  
-			class_hovered : "hovered",
+			class_main : primaryClass,
+			class_menu : primaryClass + "-menu",  
+			class_contents : primaryClass + "-contents",  
+			class_content : primaryClass + "-content", 
+			class_selected : primaryClass + "-selected",
+			class_hovered : primaryClass + "-hovered",
+			class_slideBox : primaryClass + "-slideBox",
+			class_slideBox_inner : primaryClass + "-slideBox-inner",
+			class_edge : primaryClass + "-edge",
+			defaultHeight : 40,
 			effect : "linear",
 			speed : 500,			
 			content : null,
@@ -13,34 +21,44 @@
 			} 
 		}, o || {});
 
-		return this.each(function() {
-			//$ means jQuery object	
-			//Menu 
+		return this.each(function() {//$ initial variables means jQuery object	
+				//Menu
 			var $m_target = $(this).addClass(o.class_main),
-				$m_ul = $("ul:first",this).addClass(o.class_main + "-menu"), 
+				$m_ul = $("ul:first",this).addClass(o.class_menu), 
 				$m_li = $("li", $m_ul), 				
 			
 				//Content
-				$c_div = $(o.content == null ? $("> div:first", this) : $(o.content)).addClass(o.class_main + "-contents"), 
+				$c_div = $(o.content == null ? $("> div:first", this) : $(o.content)).addClass(o.class_contents), 
 				$c_ul = $("ul:first",$c_div), 
-				$c_li = $("li",$c_ul).addClass(o.class_main + "-content"),
+				$c_li = $("li",$c_ul).addClass(o.class_content),				
+				$sb = $('<li class="' + o.class_slideBox + '"><div class="' + o.class_slideBox_inner + '"><div class="ver"></div></div></li>').appendTo($m_ul), 				
 				
-				sb = $('<li class="' + o.class_main + '-slideBox"><div class="' + o.class_main + '-slideBox-inner"><div class="ver"></div></div></li>').appendTo($m_ul), 
+				//Globals
 				seld = $m_li.eq(o.start).addClass(o.class_selected)[0], 
 				totalWidth = $m_target[0].offsetWidth,
 				itemLength = $c_li.size(), 
 				running = false, 
 				currentIndex = o.start;	
 						
-			$m_ul.prepend('<div class="' + o.class_main + '-edge-begin"></div>');
-			$m_ul.append('<div class="' + o.class_main + '-edge-end"></div>');			
+
+			$m_ul.prepend('<div class="' + o.class_edge + ' ' + o.class_edge + '-begin"></div>');
+			$m_ul.append('<div class="' + o.class_edge + ' ' + o.class_edge + '-end"></div>');			
+			$m_ul.css({ height : o.defaultHeight });
 			$m_li.each(function() {
 				var temp = $(this).children().detach();
 					m_div = $('<div class="list"><i></i></div>').append(temp).appendTo($(this));
-					m_div.width((totalWidth - $('.' + o.class_main + '-edge-end').width() * 2 )/ $m_li.length );
+					m_div.width((totalWidth - $('.' + o.class_edge).width() * 2 )/ $m_li.length );
 					m_div.children("a").attr("href","#");
+					$(this).css({ height : o.defaultHeight });
+					m_div.css({ height : o.defaultHeight });
 			});
+			$sb.css({width : seld.offsetWidth, height : o.defaultHeight, left : seld.offsetLeft});	
+			$c_div.css({width : totalWidth});
+			$c_li.css({width : totalWidth});		
+			$c_ul.css({left : 0, height : $c_li[currentIndex].offsetHeight});
+			$c_ul.css("width", itemLength * totalWidth).css("left", -(currentIndex * totalWidth));
 			
+			setSelected(seld);
 			//Menu items' key LEFT and RIGHT 
 			$(document).keydown(function(e) {
 				switch (e.keyCode) {
@@ -53,18 +71,14 @@
 				}
 			});
 			
-			
-			
 			//Menu items' mouse over 
 			$m_li.hover(function() {
 				$(this).addClass(o.class_hovered);
 			}, function() {
 				$(this).removeClass(o.class_hovered);
 			});
-			//Menu items' click slide handle
-			$m_li.not("." + o.slidebox).click(function() {
-				move(this);
-			});
+			
+			//Click function hack - handle selected
 			$m_li.click(function(e) {
 				if (this != seld) {
 					setSelected(this);
@@ -72,47 +86,36 @@
 				}
 				return o.click.apply(this, [ e, null ]);
 			});
-		
-			setSelected(seld);
-			sb.css({"width" : seld.offsetWidth + "px","left" : seld.offsetLeft + "px"});	
 			
-		
-			$c_li.css({margin : "0",padding : "0",overflow : "hidden","float" : "left"});		
-			$c_ul.css({left : "0px",height : $c_li[currentIndex].offsetHeight});						
-			var liSize = width($c_li);
-			
-					
-			$c_li.css({width : totalWidth});
-			$c_ul.css("width", itemLength * totalWidth).css("left", -(currentIndex * totalWidth));
-			$c_div.css({width : totalWidth, visibility : "visible"});
-			
-		
-			
+			//Menu items' click handle
 			$.each($m_li, function(i, val) {
 				$(val).click(function() {
-					return go(i);
+					menuSlide(this);
+					return contentSlide(i);
 				});
 			});
+			
 			function setSelected(el) {
 				$(seld).removeClass(o.class_selected);
 				seld = el;
 				$(el).addClass(o.class_selected);
 			}
 			
-			function move(el) {
-				sb.stop(false, false).animate({
+			function menuSlide(el) {
+				$sb.stop(false, false).animate({
 					width : el.offsetWidth,
 					left : el.offsetLeft
 				}, o.speed, o.effect);
 			}
-			function go(to) {
+			
+			function contentSlide(index) {
 				$c_div.css({
-					height : $c_li[to].offsetHeight
+					height : $c_li[index].offsetHeight
 				});
-				if (to < 0 || to > itemLength - 1)
+				if (index < 0 || index > itemLength - 1)
 					return;
 				else
-					currentIndex = to;
+					currentIndex = index;
 
 				running = true;
 
@@ -122,19 +125,8 @@
 					running = false;
 				});
 				return false;
-			}
-			;
-			
+			};
 		});
-		function css(el, prop) {
-			return parseInt($.css(el[0], prop)) || 0;
-		}
-		function width(el) {
-			return el[0].offsetWidth + css(el, 'marginLeft') + css(el, 'marginRight');
-		}
-		function height(el) {
-			return el[0].offsetHeight + css(el, 'marginTop') + css(el, 'marginBottom');
-		}
 	};
 	
 })(jQuery);
