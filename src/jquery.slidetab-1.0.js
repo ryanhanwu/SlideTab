692674a3df8ae8bee03f3139a0778b068d23852c
Flyworld <flyworld@gmail.com>
2011-10-19 11:10:15 +0800

    refactor menu part

--- src/jquery.slidetab-1.0.js
+++ src/jquery.slidetab-1.0.js
@@ -1,12 +1,11 @@
 (function($) {
-	$.fn.jProductMenuBox = function(o) {
+	$.fn.slideTab = function(o) {
 		o = $.extend({
+			class_main : "slideTabs",
+			class_selected : "selected",  
+			class_hovered : "hovered",
 			effect : "linear",
-			speed : 500,
-			hovered : "hovered", 
-			selected : "selected", 
-			slidebox : "slideBox", 
-			btnGo : null,
+			speed : 500,			
 			content : null,
 			start : 0,
 			scroll : 1,
@@ -15,18 +14,36 @@
 		}, o || {});
 
 		return this.each(function() {
-			var target = $(this), 
-				$li = $("li", this), 
-				sb = $('<li class="' + o.slidebox + '"><div class="' + o.slidebox + '-inner"><div class="ver"></div></div></li>').appendTo(target), 
-				$a = $("a", this), 
-				seld = $("li." + o.selected, this)[0] || $li.eq(0).addClass(o.selected)[0], 
+			//$ means jQuery object	
+				
+			var $m_target = $(this),
+				$m_ul = $("ul",this), 
+				$m_li = $("li", $m_ul), 
+			//	.appendTo($m_li),
+				//$m_li.children(a).append();
+				//$m_div = $m_li.children("div"),				
+				//Menu 
+				$c_div = $(o.content), 
+				$c_ul = $c_div.children("ul:first"), 
+				$c_li = $c_ul.children("li"),
+				//Content
+				sb = $('<li class="' + o.class_main + '-slideBox"><div class="' + o.class_main + '-slideBox-inner"><div class="ver"></div></div></li>').appendTo($m_ul), 
+				seld = $("li." + o.class_selected, this)[0] || $m_li.eq(0).addClass(o.class_selected)[0], 
 				running = false, 
-				div = $(o.content), 
-				ul = div.children("ul").eq(0), 
-				$cli = ul.children("li"), 
-				itemLength = $cli.size(), 
-				curr = o.start;
-
+				itemLength = $c_li.size(), 
+				curr = o.start;	
+				
+			$m_target.addClass(o.class_main);			
+			$m_ul.addClass(o.class_main + "-menu");
+			$m_ul.prepend('<div class="' + o.class_main + '-edge-begin"></div>');
+			$m_ul.append('<div class="' + o.class_main + '-edge-end"></div>');			
+			$m_li.each(function() {
+				var temp = $(this).children().detach();
+					m_div = $('<div class="list"><i></i></div>').append(temp).appendTo($(this));
+					m_div.width($m_target[0].offsetWidth / $m_li.length - $('.' + o.class_main + '-edge-end').width() * 2);	
+			});
+			
+			//Key LEFT and RIGHT
 			$(document).keydown(function(e) {
 				switch (e.keyCode) {
 					case 37:
@@ -38,17 +55,17 @@
 				}
 			});
 
-			$li.not("." + o.slidebox).click(function() {
+			$m_li.not("." + o.slidebox).click(function() {
 				move(this);
 			});
 			
-			$li.hover(function() {
-				$(this).addClass(o.hovered);
+			$m_li.hover(function() {
+				$(this).addClass(o.class_hovered);
 			}, function() {
-				$(this).removeClass(o.hovered);
+				$(this).removeClass(o.class_hovered);
 			});
 			
-			$li.click(function(e) {
+			$m_li.click(function(e) {
 				if (this != seld) {
 					setSelected(this);
 					return o.click.apply(this, [ e, this ]);
@@ -60,25 +77,25 @@
 			sb.css({"width" : seld.offsetWidth + "px","left" : seld.offsetLeft + "px"});	
 			
 		
-			$cli.css({margin : "0",padding : "0",overflow : "hidden","float" : "left"});		
-			div.css({left : "0px",height : $cli[curr].offsetHeight});						
-			var liSize = width($cli),		
+			$c_li.css({margin : "0",padding : "0",overflow : "hidden","float" : "left"});		
+			$c_div.css({left : "0px",height : $c_li[curr].offsetHeight});						
+			var liSize = width($c_li),		
 			ulSize = liSize * itemLength,	
 			divSize = liSize; 					
-			$cli.css({width : $cli.width()});
-			ul.css("width", ulSize + "px").css("left", -(curr * liSize));
-			div.css("visibility", "visible"); 
-			div.css("width", divSize + "px");
+			$c_li.css({width : $c_li.width()});
+			$c_ul.css("width", ulSize + "px").css("left", -(curr * liSize));
+			$c_div.css("visibility", "visible"); 
+			$c_div.css("width", divSize + "px");
 			
-			$.each($(o.btnGo), function(i, val) {
+			$.each($m_li, function(i, val) {
 				$(val).click(function() {
 					return go(i);
 				});
 			});
 			function setSelected(el) {
-				$(seld).removeClass(o.selected);
+				$(seld).removeClass(o.class_selected);
 				seld = el;
-				$(el).addClass(o.selected);
+				$(el).addClass(o.class_selected);
 			}
 			
 			function move(el) {
@@ -88,8 +105,8 @@
 				}, o.speed, o.effect);
 			}
 			function go(to) {
-				div.css({
-					height : $cli[to].offsetHeight
+				$c_div.css({
+					height : $c_li[to].offsetHeight
 				});
 				if (to < 0 || to > itemLength - 1)
 					return;
@@ -98,7 +115,7 @@
 
 				running = true;
 
-				ul.stop(false, false).animate({
+				$c_ul.stop(false, false).animate({
 					left : -(curr * liSize)
 				}, o.speed, o.effect, function() {
 					running = false;
